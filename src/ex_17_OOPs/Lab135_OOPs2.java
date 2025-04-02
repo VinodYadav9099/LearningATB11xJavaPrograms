5fbb8dc27a1e1d796f0bbed161dfca868f188ab4
VINOD YADAV <vinodyadav4480@gmail.com>
2025-04-03 02:38:18 +0530

    31-Mar-25 Practice Session and Assignment

---
+++ src/ex_17_OOPs/Lab135_OOPs2.java
@@ -0,0 +1,19 @@
+package ex_17_OOPs;
+
+public class Lab135_OOPs2 {
+    Student s1 = new Student();
+    //1. Student - Class Loading
+    //2. s1 - Object Ref.
+    //3. new Student() -> Object creation
+
+
+
+}
+
+class Student{
+    String name;
+    int roll;
+
+    void sleep(){}
+
+}
---
+++ src/ex_17_OOPs/Lab136_Class_within_Class.java
@@ -0,0 +1,21 @@
+package ex_17_OOPs;
+
+public class Lab136_Class_within_Class {
+
+    public static void main(String[] args) {
+        System.out.println("136");
+    }
+}
+
+//class Lab137_Class_within_Class {
+//    public static void main(String[] args) {
+//        System.out.println("137");
+//    }
+//}
+//
+//class Lab138_Class_within_Class {
+//    public static void main(String[] args) {
+//        System.out.println("138");
+//    }
+//}
+
